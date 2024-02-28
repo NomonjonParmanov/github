bb2d46879c9912b5d1eeeab9038d90d237561439
Nomonjonprog <ibragimovganijon354@gmail.com>
2024-02-28 14:06:11 +0300

    response

--- script/pages/main.js
+++ script/pages/main.js
@@ -1,7 +1,40 @@
-let url = "https://api.github.com/users/NomonjonParmanov";
-let img = document.getElementById("img_logo");
+let url = "https://api.github.com/users/kamronbek015";
+let input = document.querySelector(".input");
+let form = document.querySelector(".input-form");
+let repos = document.querySelector(".repos");
+
 fetch(url)
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
+    form.addEventListener("keyup", () => {
+      let inputValue = input.value.toLowerCase();
+      fetch(data.repos_url)
+        .then((res) => res.json())
+        .then((reposData) => {
+          let filteredRepos = reposData.filter((repo) =>
+            repo.name.toLowerCase().includes(inputValue)
+          );
+          displayRepos(filteredRepos);
+        });
+    });
+    displayUser(data);
   });
+
+function displayUser(data) {
+  repos.innerHTML = `
+                <img class="ava" src="${data.avatar_url}" alt="">
+                <p>${data.login}</p>
+            `;
+}
+
+function displayRepos(filteredRepos) {
+  repos.innerHTML = filteredRepos
+    .map(
+      (repo) => `
+                <div class="repo">
+                    <p>${repo.name}</p>
+            `
+    )
+    .join("");
+}
